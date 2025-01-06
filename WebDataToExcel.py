79de3d541534e90e61cf0ee54bfccf83238f024e
Ranindu Nethmina <ranindunethmina@gmail.com>
2025-01-06 13:07:24 +0530

    Refactor File Names

--- WebDataToExcel.py
+++ WebDataToExcel.py
@@ -7,7 +7,6 @@ url = "https://www.geeksforgeeks.org/fundamentals-of-algorithms/"
 response = requests.get(url)
 
 if response.status_code == 200:
-
     soup = BeautifulSoup(response.content, 'html.parser')
 
     headings = []
@@ -17,7 +16,6 @@ if response.status_code == 200:
         heading_text = heading.text.strip()
 
         next_paragraph = heading.find_next('p')
-
         headings.append(heading_text)
         if next_paragraph:
             content.append(next_paragraph.text.strip())
@@ -27,8 +25,6 @@ if response.status_code == 200:
     df = pd.DataFrame({'Heading': headings, 'Content': content})
 
     df.to_excel("scramp.xlsx", sheet_name="Headings and Content", index=False)
-
     print("Data writed")
-
 else:
-    print('Error')
+    print("Error")
--- WebImgToFolder.py
+++ WebImgToFolder.py
@@ -2,39 +2,27 @@ import os
 import requests
 from bs4 import BeautifulSoup
 import urllib.request
-from PIL import Image
 
-# URL of the webpage
 url = "https://www.geeksforgeeks.org/fundamentals-of-algorithms/"
 
-# Send a GET request to fetch the page content
 response = requests.get(url)
 
-# Ensure the response is successful
 if response.status_code == 200:
-    # Parse the page content
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find all 'img' tags
     tags = soup.find_all('img', alt="What is Algorithm?")
 
-    # Create a directory to store the images
     if not os.path.exists('downloaded_images'):
         os.makedirs('downloaded_images')
 
-    # Iterate over the image tags and download them
     for index, tag in enumerate(tags):
-        # Get the 'src' attribute (image URL)
         img_url = tag.get('src')
 
-        # Handle relative URLs by combining them with the base URL
         if not img_url.startswith("http"):
             img_url = urllib.parse.urljoin(url, img_url)
 
-        # Define the path to save the image
         img_path = f"downloaded_images/image_{index}.jpg"
 
-        # Download and save the image
         try:
             urllib.request.urlretrieve(img_url, img_path)
             print(f"Downloaded: {img_url}")
